0726dcd341f6d76580f7552a51185d0c7643c0ad
NagaSwaroop2611 <swaroopdayanandasagar@gmail.com>
2025-01-23 17:54:48 +0530

    Assignment48 Pushed

---
+++ Assignment48/script.js
@@ -0,0 +1,5 @@
+function changeStyle() {
+  document.querySelector(".para1").className = "newPara1";
+  document.querySelector(".para2").className = "newPara2";
+  document.querySelector(".para3").className = "newPara3";
+}
